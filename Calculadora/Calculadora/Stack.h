90cb0c96a8e2735e0a88e5779ffb9994a9195580
Jose Isaac <jisaac19-1@hotmail.com>
2018-09-22 12:32:47 -0600

    cambios
    data structures

--- Calculadora/Calculadora/Stack.h
+++ Calculadora/Calculadora/Stack.h
@@ -3,41 +3,41 @@
 #include <iostream>
 template<class T>
 struct nodeS {
-	const T* data;
+	T data;
 	struct nodeS* next;
 };
 template <class T>
 class Stack {
 private:
-	 nodeS<T>* top;
+	nodeS<T>* top;
 	bool isEmpty();
 public:
 	Stack();
 	~Stack();
-	void push(const T  *);
-	const T* pop();
-	T* peek();
+	void push(const T  &);
+	const T& pop();
+	const T& peek();
 };
 #endif 
 
 template<class T>
-inline bool Stack<T>::isEmpty(){
-	return !top;
+inline bool Stack<T>::isEmpty() {
+	return top == nullptr;
 }
 
 template<class T>
-inline Stack<T>::Stack(){
+inline Stack<T>::Stack() {
 	top = nullptr;
 }
 
 template<class T>
-inline Stack<T>::~Stack(){
+inline Stack<T>::~Stack() {
 
 }
 
 template<class T>
-inline void Stack<T>::push(const T* item) {
-	 nodeS<T>* nuevo;
+inline void Stack<T>::push(const T& item) {
+	nodeS<T>* nuevo;
 	try {
 		nuevo = new nodeS<T>;
 	}
@@ -50,21 +50,21 @@ inline void Stack<T>::push(const T* item) {
 }
 
 template<class T>
-inline const T* Stack<T>::pop(){
-	if (isEmpty())
-		return nullptr;
-	 const T* _data;
-	nodeS<T>* tmp;
-	_data = top->data;
-	tmp = top;
-	top = top->next;
-	delete tmp;
-	tmp = nullptr;
-	return _data;
+inline const T& Stack<T>::pop() {
+	if (!isEmpty()) {
+		T _data;
+		nodeS<T>* tmp;
+		_data = top->data;
+		tmp = top;
+		top = top->next;
+		delete tmp;
+		tmp = nullptr;
+		return _data;
+	}
+
 }
 
 template<class T>
-inline T * Stack<T>::peek(){
-	return (!isEmpty()? top->data:nullptr);
+inline const T & Stack<T>::peek() {
+	return (!isEmpty() ? top->data : nullptr);
 }
-
